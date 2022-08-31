0cb8ebfa28bb6cadb1ae4ac8e871353323a1b154
IvanRomero03 <i.wells.ar@gmail.com>
2022-08-31 10:00:53 -0500

    upsi

--- pages/undefined.tsx
+++ pages/undefined.tsx
@@ -1,8 +1,10 @@
+import { Spinner } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 const undefinedPage = () => {
   const router = useRouter();
   router.push("/");
+  return <Spinner />;
 };
 
 export default undefinedPage;

d09a270c1ba56972f60c62098058edca0f7b7bb3
IvanRomero03 <i.wells.ar@gmail.com>
2022-08-30 22:15:21 -0500

    try production

--- client.ts
+++ client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://robo-links.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
